5a6408e71b5a0d69d4da146e3c2fc43348824626
Si Ming Zhou <elginsi2000@gmail.com>
2024-06-12 00:10:07 +0800

    first commit - setup and added navbar

---
+++ client/src/main.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.tsx";
+import Login from "./components/Login.tsx";
+import Watchlist from "./components/Watchlist.tsx";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/watchlist",
+        element: <Watchlist />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
